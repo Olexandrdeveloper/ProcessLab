4e80eb6d68cb0f1d4c2917ca08ea517479cfdfa9
Саша O. <si.ovcharuk2@gmail.com>
2026-09-22 20:39:27 +0300

    Add project files.

---
+++ ProcessLab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace ProcessLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    Console.WriteLine($"ID: {process.Id, -5} | Назва: {process.ProcessName}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Помилка доступу до процесу: {ex.Message}");
+                }
+            }
+        }
+    }
+}
